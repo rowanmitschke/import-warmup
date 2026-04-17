705d0b7470d3bf75364f3fa1d8e762f864e5ede5
Rowan Mitschke <133076215+rowanmitschke@users.noreply.github.com>
2026-04-17 15:46:29 +0000

    exercise_1

--- run_exercises.py
+++ run_exercises.py
@@ -1,3 +1,5 @@
+import portfolio.data, portfolio.report
+
 if __name__ == "__main__":
     my_portfolio = portfolio.data.create_portfolio("Retirement")
     portfolio.report.print_report(my_portfolio)
