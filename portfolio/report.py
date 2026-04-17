f2f5a2390a1a28b6e29715c44aa72bff0dd07e79
Rowan Mitschke <133076215+rowanmitschke@users.noreply.github.com>
2026-04-17 15:49:15 +0000

    exercise_2

--- portfolio/report.py
+++ portfolio/report.py
@@ -1,3 +1,5 @@
+from portfolio.metrics import calculate_portfolio_value
+
 def print_report(portfolio: list[dict]) -> None:
     total = calculate_portfolio_value(portfolio)
     print(f"PORTFOLIO SUMMARY\nPortfolio Name: {portfolio["name"]}")
@@ -5,11 +7,3 @@ def print_report(portfolio: list[dict]) -> None:
     print(f"Number of assets: {len(portfolio["assets"])}")
     print(f"Total value: ${total:,.2f}")
 
-def calculate_portfolio_value(portfolio: dict) -> float:
-    total = 0
-    for asset in portfolio["assets"]:
-        total += calculate_asset_value(asset)
-    return total
-
-def calculate_asset_value(asset: dict) -> float:
-    return asset["price"] * asset["quantity"]
